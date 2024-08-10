759df35bbe1b27a9086fb6552a6df4007a4959c3
oddtO <121516833+oddtO@users.noreply.github.com>
2024-08-10 19:35:13 +0300

    format message date

--- controllers/index.ts
+++ controllers/index.ts
@@ -1,11 +1,19 @@
 import type { Request, Response } from "express";
 import { messages } from "../data";
-
+import { format } from "date-fns";
 type Flatten<Type> = Type extends Array<infer Item> ? Item : never;
 
 type RequestBody = {
   username: string;
   message: string;
+};
+
+// Save the original toString method
+// const originalToString = Date.prototype.toString;
+
+// Override the Date.prototype.toString method
+Date.prototype.toString = function () {
+  return format(this, "h:mm - EEE MMM d yyyy");
 };
 
 function get(req: Request, res: Response) {
