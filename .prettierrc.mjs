9591471f9fb002431596d3656a8f4f9cb7431a9f
oddtO <121516833+oddtO@users.noreply.github.com>
2024-11-11 07:42:24 +0200

    add open graph data

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,6 @@
 /** @type {import('prettier').Config} */
 const prettierConfig = {
-  plugins: ["prettier-plugin-embed", "prettier-plugin-sql"],
+  plugins: ["prettier-plugin-embed", "prettier-plugin-sql", "prettier-plugin-ejs"],
 };
 
 /** @type {import('prettier-plugin-embed').PrettierPluginEmbedOptions} */
